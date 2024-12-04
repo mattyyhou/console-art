94da1675e41b9c574d4bfa2bf674be0585c82425
Matthew Houston <matthewhouston1@gmail.com>
2024-12-04 16:16:25 +0100

    updated variables and function names

--- art.js
+++ art.js
@@ -1,17 +1,17 @@
 // TODO Add functionality for drawing shapes
-function drawLineOfStars(length) {
-    let rowOfStars = "";
-    for (let starCount = 0; starCount < length; starCount++) {
-      rowOfStars = rowOfStars + "*";
+function drawShape(length) {
+    let rowOfShape = "";
+    for (let shapeCount = 0; shapeCount < length; shapeCount++) {
+      rowOfShape = rowOfShape + "*";
     }
-    console.log(rowOfStars)
+    console.log(rowOfShape)
   }
   
-  function drawTriangleWithHeight(height) {
+  function drawShapeWithHeight(height) {
     for (let rowCount = 1; rowCount < height + 1; rowCount++) {
-      drawLineOfStars(rowCount)
+      drawShape(rowCount)
     }
   }
   
-  drawTriangleWithHeight(5)
+  drawShapeWithHeight(5)
   
