79cbe82f984637c971c36a62c75835a0acba11d0
Matthew Houston <matthewhouston1@gmail.com>
2024-12-04 16:11:31 +0100

    added triangle with functions code

--- art.js
+++ art.js
@@ -1,1 +1,17 @@
 // TODO Add functionality for drawing shapes
+function drawLineOfStars(length) {
+    let rowOfStars = "";
+    for (let starCount = 0; starCount < length; starCount++) {
+      rowOfStars = rowOfStars + "*";
+    }
+    console.log(rowOfStars)
+  }
+  
+  function drawTriangleWithHeight(height) {
+    for (let rowCount = 1; rowCount < height + 1; rowCount++) {
+      drawLineOfStars(rowCount)
+    }
+  }
+  
+  drawTriangleWithHeight(5)
+  
