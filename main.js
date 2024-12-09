0ca0158d856bb5db71d46e297d7c832747bb64ac
Matthew Houston <matthewhouston1@gmail.com>
2024-12-09 17:03:58 +0100

    added if else statements to run art functions

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const { askForShape, askForDimension } = require('./input') 
+const { drawTriangleWithHeight, drawPyramid, drawInvertedPyramid, square } = require('./art')
 
 async function main() {
     const shape = await askForShape()
     const dimension = await askForDimension()
 
     // TODO Use input to draw shapes
+if  (shape === 'square') {
+    square(dimension);
+} else if (shape === 'triangle'){
+    drawTriangleWithHeight(dimension);
+} else if (shape === 'pyramid'){
+    drawPyramid(dimension);
+} else if (shape === 'uPyramid'){
+    drawInvertedPyramid(dimension);
+}
 }
 
-main()
+main();
